4fc7f7b57580ed8a3b5f89dc946ca580f0edf487
Nixon Duong <nixonduong@Nixons-MacBook-Pro.local>
2018-11-24 14:37:23 -0800

    Updated Graph.c and Graph.h

---
+++ pa4/Graph.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Graph.h"
+
+#define WHITE -1
+#define GRAY 0
+#define BLACK 1
+
+typedef struct GraphObj{
+	List *adjList;
+	int *color;
+	int *distance;
+	int *parent;
+	int order;
+	int size;
+	int source;
+};
+
+/* Constructors - Destructors */
+Graph newGraph(int n){ // returns a Graph pointing to a newly created GraphObj representing a graph having n vertices and no edges
+	Graph graph = malloc(sizeof(GraphObj));
+	graph->adjList = calloc(n+1, List);
+	graph->color = calloc(n+1, int);
+	graph->distance = calloc(n+1, int);
+	graph->parent = calloc(n+1, int);
+	graph->order = n;
+	graph->size = 0;
+	graph->source = NIL;
+	return (graph);
+}
+
+void freeGraph(Graph *pG){ // frees all dynamic memory associated with the Graph *pG, then sets *pG to NULL
+
+}
+
+/*Access functions*/
+int getOrder(Graph G){ // returns corresponding field value
+	if(G == NULL){
+		fprintf(stderr, "getOrder() called on NULL graph reference");
+		exit(1);
+	}
+	else{
+		return (G->order);
+	}
+}
+
+int getSize(Graph G){ // returns corresponding field value
+	if(G == NULL){
+		fprintf(stderr, "getSize() called on NULL graph reference");
+		exit(1);
+	}
+	else{
+		return (G->size);
+	}
+}
+
+int getSource(Graph G){ // returns source vertex most recently used in BFS() or NIL if BFS() has not been called yet
+	if(G == NULL){
+		fprintf(stderr, "getSource() called on NULL graph reference");
+		exit(1);
+	}
+	else if(getSource(G) != NIL){
+		return (G->source);
+	}
+	else{
+		return (NIL);
+	}
+}
+
+int getParent(Graph G, int u){ // Pre: 1<=u<=getOrder(G) returns parent of u
+	if(G == NULL){
+		fprintf(stderr, "getParent() called on NULL graph reference");
+		exit(1);
+	}
+	else if(u >= 1 && u <= getOrder(G)){
+		return (G->parent[u]);
+	}
+	else{
+		fprintf(stderr, "getParent() called on vertex out of bounds");
+		exit(1);
+	}
+}
+
+int getDist(Graph G, int u){ // Pre: 1<=u<=getOrder(G) returns distance from most recent BFS source to u or INF if no path
+	if(G == NULL){
+		fprintf(stderr, "getDist() called on NULL graph reference");
+		exit(1);
+	}
+	else if(u == getSource(G)){
+		return (NIL);
+	}
+	else if(u >= 1 && u <= getOrder(G)){
+		return (G->distance[u]);
+	}
+	else{
+		return (INF);
+	}
+}
+
+void getPath(List L, Graph G, int u){ // appends to the List L the vertices of a shortest path in G from source u, or appends to L the value NIL if no such path exist Pre: getSource(G) != NIL && 1<=u<=getOrder(G)
+	if(G == NULL){
+		fprintf(stderr, "getPath() called on NULL graph reference");
+		exit(1);
+	}
+	else if(getSource(G) != NIL && 1 <= u && u <= getOrder(G)){
+			if(u == getSource(G)){
+				prepend(u);
+			}
+			else if(getParent(G,u) == NIL){
+				append(L, NIL);
+			}
+			else{
+				getPath(L,G,getParent(G,u));
+				prepend(L,u);
+			}
+		}
+	}
+	else{
+		fprintf(stderr, "getPath() called on NIL source/vertex out of bounds");
+		exit(1);
+	}
+}
+
+/*Manipulation procedures*/
+void makeNull(Graph G){ // deletes all edges of G, restoring it to its original (no edge) state
+	if(G == NULL){
+		fprintf(stderr, "makeNull() called on NULL graph reference");
+		exit(1);
+	}
+	for(int i = 1; i <= G->size + 1; i++){
+		clear(G->adjList[i]);
+	}
+}
+
+void addEdge(Graph G, int u, int v){ // inserts a new edge joining u to v (u is added to the adjacency List of v and v to the adjacency list of u)
+
+}
+
+void addArc(Graph G, int u, int v){ // inserts a new directed edge (v is added to the adjacency list of u, but not u to v)
+
+}
+
+void BFS(Graph G, int s){ // runs BFS on G
+
+}
+
+/* Other operations */
+void printGraph(FILE* out, Graph G){ // prints the adjacency list representation of G to the file pointed to by out
+
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
