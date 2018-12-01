774748f004d264e16ec95dc0f07a126cc7c0cb01
Nixon Duong <nixonduong@Nixons-MacBook-Pro.local>
2018-12-01 15:39:04 -0800

    Graph.h update

---
+++ pa5/Graph.h
@@ -0,0 +1,31 @@
+#ifndef _GRAPH_H_INCLUDE_
+#define _GRAPH_H_INCLUDE_
+#define NIL -1
+#define UNDEF -2
+#include "List.h"
+
+/*Exported type*/
+typedef struct GraphObj* Graph;
+
+/*Constructors-Deconstructors*/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+
+/*Access functions*/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getParent(Graph G, int u);
+int getDiscover(Graph G, int u);
+int getFinish(Graph G, int u);
+
+/*Manipulation functions*/
+void addArc(Graph G, int u, int v);
+void addEdge(Graph G, int u, int v);
+void DFS(Graph G, List S);
+
+/*Other functions*/
+Graph transpose(Graph G);
+Graph copyGraph(Graph G);
+void printGraph(FILE* out, Graph G);
+
+#endif
